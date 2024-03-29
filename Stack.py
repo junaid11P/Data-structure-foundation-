b5305bd5f6cfe6314182c330249cb3fddf20c38f
juned <79745275+junaid11P@users.noreply.github.com>
2024-03-30 00:49:49 +0530

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,35 @@
+class stack:
+  stack=[]
+
+  def is_empty(self):
+    return len(self.stack) == 0
+
+  def push(self, item):
+    self.stack.append(item)
+
+  def pop(self):
+    if self.is_empty():
+      print("Stack is empty. Cannot pop.")
+    else:
+      return self.stack.pop()
+            
+  def peek(self):
+    if self.is_empty():
+      print("Stack is empty. Cannot peek.")
+    else:
+      return self.stack[-1]
+
+  def size(self):
+    return len(self.stack)
+
+if __name__=='__main__':
+  stack=stack()
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  print("The elements are:",stack.stack())
+  stack.pop()
+  print("The elements are:",stack.stack())
+  print("The top most element in the stack:",stack.peek())
+  print("The size of the stack:",stack.size())
+      
