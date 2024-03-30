9be48c1e70cad93c5317da1bf36ae02d579c2273
juned <79745275+junaid11P@users.noreply.github.com>
2024-03-30 06:10:40 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,40 +1,44 @@
 class Queue:
-  def __init__(self):
-    self.queue=[]
+    def __init__(self):
+        self.queue = []
+        self.front = -1
+        self.rear = -1
 
-	def is_empty(self):
-    return len(self.queue)==0
-  def is_full(self):
-    return self.rear==len(self.queue)
-    
-  def enqueue(self,item):
-  	if self.is_full():
-      print("The queue is full")
-    else:
-    	if self.front==-1:
-      	self.front=0
-      rear+=1
-      self.queue[self.rear]=item
+    def is_empty(self):
+        return len(self.queue) == 0
 
-	def dequeue(self):
-    if self.is_empty():
-      print("The queue is empty")
-		else:
-			return self.queue[self.front]=-1
+    def is_full(self):
+        return self.rear == len(self.queue) - 1
 
-	def peek(self):
-    if self.is_empty():
-      print("The queue is empty")
-  else:
-		return self.front
-        
-if __name__=="__main__":
-  enqueue(1)
-  enqueue(2)
-  enqueue(3)
-  print("The elements are:",queue.queue)
-  dequeue()
-  print("The elements are:",queue.queue)
-  enqueue(10)
-  print("The top most element of queue",queue.peek())
-  print("The elements are:",queue.queue)
+    def enqueue(self, item):
+        if self.is_full():
+            print("The queue is full")
+        else:
+            if self.front == -1:
+                self.front = 0
+            self.rear += 1
+            self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            print("The queue is empty")
+        else:
+            self.front += 1
+
+    def peek(self):
+        if self.is_empty():
+            print("The queue is empty")
+        else:
+            return self.queue[self.front]
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    print("The elements are:", queue.queue)
+    queue.dequeue()
+    print("The elements are:", queue.queue)
+    queue.enqueue(10)
+    print("The top most element of queue", queue.peek())
+    print("The elements are:", queue.queue)
